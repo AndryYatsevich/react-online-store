3d691a907c5d0d1f66e6e3d94214463a11c9ebc4
Andrey Yatsevich <yatsevichao@gmail.com>
2018-01-26 16:53:33 +0400

    material-ui added

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -3,21 +3,34 @@ import {connect} from 'react-redux';
 import {getMenuItem} from '../../selectors/menuSelectors';
 import {loadMenuItem} from "../../action/menuAction";
 
-class Menu extends React.Component {
+import Paper from 'material-ui/Paper';
+import MenuItem from 'material-ui/MenuItem'
 
+class Menu extends React.Component {
+    constructor(props){
+        super(props);
+        const style = () => { {
+            display: 'inline-block',
+            margin: '16px 32px 16px 0'}
+        };
+    }
     componentDidMount() {
         this.props.loadMenuItem();
     }
 
 
+/*    */
+
     render() {
         return (
             <div>
-                {
-                    this.props.menuItem.map(function(el){
+                <Paper style={this.style}>
+
+                    <MenuItem primaryText={this.props.menuItem.map(function(el){
                         return <a><div key={el.id} className={'menu-item'}>{el.name}</div></a>
-                    })
-                }
+                    })} />
+
+                </Paper>
             </div>
 
         );
